4adea74e3cd2825bc1bd180224aa21b3e823ffb7
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-01 14:36:16 +0300

    Updated-2.

--- CPPFirstAPP.cpp
+++ CPPFirstAPP.cpp
@@ -6,34 +6,38 @@ class Apple
 		Apple()
 		{
 			color_apple = (char*)"Красный";
-			cost = 0.1f;
+			cost_apple = 0.1f;
 		}
 		Apple(char* _color_apple, float _cost)
 		{
 			color_apple = _color_apple;
-			cost = _cost;
+			cost_apple = _cost;
 		}
 		void show_info()
 		{
-			std::cout << "Цвет яблока: " << color_apple << "\n" << "Цена яблока: " << cost << "\n";
+			std::cout << "Цвет яблока: " << color_apple << "\n" << "Цена яблока: " << cost_apple << "\n";
 		}
-		//set-ер	(установить значение)
-		void set_cost(float _cost)
+		//	set
+		void set_cost_apple(float _cost_apple)
 		{
-			cost = _cost;
+			cost_apple = _cost_apple;
 		}
 		void set_color_apple(char* _color_apple)
 		{
 			color_apple = _color_apple;
 		}
-		//get-ер	(получить значение)
+		//	get
 		char* get_color_apple()
 		{
 			return color_apple;
 		}
+		float get_cost_apple()
+		{
+			return cost_apple;
+		}
 	private:
 		char* color_apple;
-		float cost;
+		float cost_apple;
 };
 
 int main()
@@ -42,17 +46,12 @@ int main()
 	Apple apple((char*)"Зелёный", 0.99);
 	apple.show_info();
 
-	/*test.cost = 1;
-	test.color_apple = 'c';*/
-
-	//set-ора
-	apple.set_cost(2.33);
+	//	set
+	apple.set_cost_apple(2.33);
 	apple.set_color_apple((char*)"Красный");
-	//get-ора
+	//	get
 	std::cout << apple.get_color_apple() << "\n";
+	std::cout << apple.get_cost_apple() << "\n";
 
 	apple.show_info();
 }
-
-
-
